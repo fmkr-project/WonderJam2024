9c46fc103e97660812d3d9c1085e21834afa7229
Lumvert42 <samuel.guebel@gmail.com>
2024-10-12 23:50:59 +0200

    Module Scriptable Object + update Module
    Added a Scriptable Object to create modules, and two empty module objects.

--- Assets/Scripts/Modules/Module.cs
+++ Assets/Scripts/Modules/Module.cs
@@ -2,11 +2,11 @@ using UnityEngine;
 
 namespace Modules
 {
-    public class Module : MonoBehaviour
+    public class Module
     {
         #region References
         
-        public string Name;
+        public string ModuleName;
         public Sprite Sprite;
         public int RequiredCrew;
         public ModuleClass ModuleClass;
@@ -17,6 +17,35 @@ namespace Modules
         
         public ResourceAmount Price { get; set; }
 
+        #endregion
+        
+        #region Constructors
+
+        /// <summary>
+        /// Default constructor.
+        /// </summary>
+        protected Module()
+        {
+            
+        }
+        
+        /// <summary>
+        /// Constructor with parameters.
+        /// </summary>
+        /// <param name="moduleName"></param>
+        /// <param name="sprite"></param>
+        /// <param name="requiredCrew"></param>
+        /// <param name="moduleClass"></param>
+        /// <param name="price"></param>
+        public Module(string moduleName, Sprite sprite, int requiredCrew, ModuleClass moduleClass, ResourceAmount price)
+        {
+            ModuleName = moduleName;
+            Sprite = sprite;
+            RequiredCrew = requiredCrew;
+            ModuleClass = moduleClass;
+            Price = price;
+        }
+        
         #endregion
         
         #region Methods
--- Assets/Scripts/Resource.cs
+++ Assets/Scripts/Resource.cs
@@ -11,6 +11,7 @@ public enum Resource
     Ether
 }
 
+[Serializable]
 public struct ResourceAmount
 {
     public Resource Resource;
