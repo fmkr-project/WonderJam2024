bf35bc64e37d7d2f2930116e2cefdb8bc56fb2f5
Darkgiant24 <43791371+Darkgiant24@users.noreply.github.com>
2024-10-12 13:23:31 -0100

    GameManager
    Push GameManager

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public static class GameManager
+{
+    
+    public static int crewCount;  
+    public static int moneyAmount; 
+
+    
+    public static void SaveData()
+    {
+        PlayerPrefs.SetInt("CrewCount", crewCount); 
+        PlayerPrefs.SetInt("MoneyAmount", moneyAmount); 
+        PlayerPrefs.Save();
+        Debug.Log("Données sauvegardées.");
+    }
+
+   
+    public static void LoadData()
+    {
+        crewCount = PlayerPrefs.GetInt("CrewCount", 0); 
+        moneyAmount = PlayerPrefs.GetInt("MoneyAmount", 0); 
+        Debug.Log("Données chargées : Équipage = " + crewCount + ", Argent = " + moneyAmount);
+    }
+
+    
+    public static void ResetData()
+    {
+        PlayerPrefs.DeleteKey("CrewCount"); 
+        PlayerPrefs.DeleteKey("MoneyAmount"); 
+        Debug.Log("Données réinitialisées.");
+    }
+}
