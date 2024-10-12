ce1f54ca17b1d576c941096a62b3bc55b56fa081
Lumvert42 <samuel.guebel@gmail.com>
2024-10-12 06:45:12 +0200

    Ships
    Contains the initialization of the ships scripts.

---
+++ Assets/Scripts/Ships/BossShip.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+namespace Ships
+{
+    public class BossShip : EnemyShip
+    {
+        // Start is called before the first frame update
+        private void Start()
+        {
+            
+        }
+
+        // Update is called once per frame
+        private void Update()
+        {
+
+        }
+        
+        /// <summary>
+        /// Triggers the ship's death.
+        /// Rolls on the LootTable and drops the loot.
+        /// Also generates next level or triggers the end of the game.
+        /// This method is called when the ship's health is less than or equal to 0.
+        /// </summary>
+        internal new void ShipDeath()
+        {
+            Destroy(gameObject);
+        }
+    }
+}
