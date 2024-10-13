2aa3df5fbe6c4e4c63a844f1d94cf276ecf8a729
Darkgiant24 <43791371+Darkgiant24@users.noreply.github.com>
2024-10-13 18:37:17 -0100

    dialogue

--- Assets/TutoDialog.cs
+++ Assets/TutoDialog.cs
@@ -17,7 +17,9 @@ public class TutoDialog : MonoBehaviour
             // Before and after each operation, insert this line to wait until the player clicks on the screen
             while (DialogueManager._blockDialoguePrinting) yield return new WaitForSeconds(Time.deltaTime);
             // Push a new dialogue, specifying who is speaking (enemy or player / narration)
-            DialogueManager.PushDialogue("You're on your way to discover the galaxy. Be aware, lots of pirates will try to attack you during your journey. May I suggest you go in the shop and buy some weapons to protect you?", DialogueParts.Player);
+            DialogueManager.PushDialogue("You're on your way to discover the galaxy. Be aware, lots of pirates will try to attack you during your journey.", DialogueParts.Player);
+            while (DialogueManager._blockDialoguePrinting) yield return new WaitForSeconds(Time.deltaTime);
+            DialogueManager.PushDialogue("May I suggest you go in the shop and buy some weapons to protect you?", DialogueParts.Player);
             while (DialogueManager._blockDialoguePrinting) yield return new WaitForSeconds(Time.deltaTime);
             DialogueManager.PushDialogue("You will find it not far from here. Use your money to have defenses and weapons", DialogueParts.Player);
             while (DialogueManager._blockDialoguePrinting) yield return new WaitForSeconds(Time.deltaTime);
