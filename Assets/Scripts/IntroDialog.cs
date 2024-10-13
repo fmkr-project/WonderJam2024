54aa507c317d93e0e3958b6d39b8a9b86397e4fb
Darkgiant24 <43791371+Darkgiant24@users.noreply.github.com>
2024-10-13 16:59:19 -0100

    push

--- Assets/Scripts/IntroDialog.cs
+++ Assets/Scripts/IntroDialog.cs
@@ -14,34 +14,11 @@ public class IntroDialog : MonoBehaviour
         // Before and after each operation, insert this line to wait until the player clicks on the screen
         while (DialogueManager._blockDialoguePrinting) yield return new WaitForSeconds(Time.deltaTime);
         // Push a new dialogue, specifying who is speaking (enemy or player / narration)
-        DialogueManager.PushDialogue("Hurry Up Cap'tain, we need to leave! Use our weapon to attack ", DialogueParts.Player);
-            while (DialogueManager._blockDialoguePrinting) yield return new WaitForSeconds(Time.deltaTime);
-            StartCoroutine(DialogueManager.CloseDialogueBox());
+        DialogueManager.PushDialogue("Hurry Up Cap'tain, we need to leave! Use our weapon to attack their shield and let's flee!", DialogueParts.Player);
+        while (DialogueManager._blockDialoguePrinting) yield return new WaitForSeconds(Time.deltaTime);
+        DialogueManager.PushDialogue("They can't escape! Their ship is ours!", DialogueParts.Enemy);
+        while (DialogueManager._blockDialoguePrinting) yield return new WaitForSeconds(Time.deltaTime);
+        StartCoroutine(DialogueManager.CloseDialogueBox());
         }
-        else
-        {
-            GenerateLoot();
-            
-            foreach (var dico in Loot)
-            {
-                FindObjectOfType<PlayerShip>().AddResourceToInventory(dico.Key,dico.Value);
-                switch (dico.Key)
-                {
-                    case Resource.Crew:
-                        StartCoroutine(DialogueManager.OpenLootBox(spriteCrew,"Un nouveau membre !"));
-                        break;
-                    case Resource.Money:
-                        StartCoroutine(DialogueManager.OpenLootBox(spriteMoney,dico.Value+" Pièces pour toi !" ));// TODO : changer les textes
-                        break;
-                    case Resource.Scrap:
-                        StartCoroutine(DialogueManager.OpenLootBox(spriteScrap,dico.Value + " Pièces de ferailles !"));
-                        break;
-                }
-                while (DialogueManager._blockLootClosing) yield return new WaitForSeconds(Time.deltaTime);
-                StartCoroutine(DialogueManager.CloseLootBox());
-                yield return new WaitForSeconds(1);
-            }
-        }
-
+       
     }
-}
