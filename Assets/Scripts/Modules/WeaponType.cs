9ead28af9f009549757b02a7e4615b630b44d95f
Lumvert42 <samuel.guebel@gmail.com>
2024-10-13 05:28:57 +0200

    Weapon Scriptable Objects
    Added many weapon scriptable objects. Maybe it is balanced, maybe not.

--- Assets/Scripts/Modules/WeaponType.cs
+++ Assets/Scripts/Modules/WeaponType.cs
@@ -2,8 +2,12 @@
 {
     public enum WeaponType
     {
-        Laser,
-        Missiles,
-        Torpedo
+        Laser, // Basic single-target weapon.
+        PlasmaThrower, // Basic all-target weapon.
+        Disruptor, // Anti-shield single-target weapon. Only deals damage if the target has shields.
+        ArcEmitter, // Anti-shield all-target weapon. Only deals damage if the target has shields.
+        Autocannon, // Repetitive random-target weapon.
+        Missiles, // Single-target weapon that multiplies its damage by two if the target has no shields.
+        Torpedoes // All-target weapon that multiplies its damage by two if the target has no shields.
     }
 }
